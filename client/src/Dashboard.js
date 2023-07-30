838962bea58595d7773ae9583b6840b4ee116083
Samana <70138609+Raven49@users.noreply.github.com>
2023-07-30 14:20:12 +0530

    Update Dashboard.js

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,36 +1,28 @@
-body{
-  background-color: rgb(196, 222, 135);
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import jwt from 'jsonwebtoken';
+
+const Dashboard = () => {
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      localStorage.removeItem('token');
+      navigate('/login');
+    } else {
+      const decodedUser = jwt.decode(token);
+      setUser(decodedUser);
+    }
+  }, [navigate]);
+
+  return (
+    <div>
+      <h1>Hello {user ? user.name : "No user found"}</h1>
+    </div>
+  );
 }
-div{
-  display: block;
-}
-h1{
-  text-align: center;
-  text-decoration: double;
-}
-input{
-  display: block;
-  width: 50;
-  margin: 10%;
-  text-align: center;
-  padding: 6px;
-}
-button{
-  display: block;
-  width: 20%;
-  margin: 12%;
-  padding-top: 5px;
-  padding-bottom: 5px;
-  padding-left: 20px;
-  padding-right: 20px;
-  text-align: center;
-  color: aliceblue;
-  background-color:black;
-  border-radius: 10px;
-  border-color: black;
-}
-form{
-  width: 50%;
-  position: relative;
-  margin-left: 40%;
-}
+
+export default Dashboard;
+
